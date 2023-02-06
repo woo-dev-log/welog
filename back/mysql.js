82bad02c08a45d01bfb7732b5e8281c86bed6fa9
woohyuk <s_swwhh@naver.com>
2023-02-07 01:03:41 +0900

    pc db연동 수정

--- back/mysql.js
+++ back/mysql.js
@@ -1,9 +1,17 @@
 const mysql = require('mysql2/promise');
 
+// module.exports = mysql.createPool({
+//     host: '127.0.0.1',
+//     port: 3306,
+//     user: 'root',
+//     password: '',
+//     database: 'welogDB'
+// })
+
 module.exports = mysql.createPool({
-    host: '127.0.0.1',
+    host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '',
+    password: '1111',
     database: 'welogDB'
 })

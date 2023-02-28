5ef42a6f3f5409119013ded0cac4837d27ed2a8c
woo-dev-log <sswwhh.dev@gmail.com>
2023-02-28 16:15:43 +0900

    heroku clearDB 연동

--- back/mysql.js
+++ back/mysql.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql2/promise');
 
-module.exports = mysql.createPool({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: "",
-    database: 'welogDB'
-})
+// module.exports = mysql.createPool({
+//     host: '127.0.0.1',
+//     port: 3306,
+//     user: 'root',
+//     password: "",
+//     database: 'welogDB'
+// })
 
 // module.exports = mysql.createPool({
 //     host: 'localhost',
@@ -15,3 +15,11 @@ module.exports = mysql.createPool({
 //     password: '1111',
 //     database: 'welogDB'
 // })
+
+module.exports = mysql.createPool({
+    host: 'us-cdbr-east-06.cleardb.net',
+    port: 3306,
+    user: 'b34b9a57a49659',
+    password: "f94a3205",
+    database: 'heroku_f2a4e125971c6a2'
+})

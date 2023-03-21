049bdaa4568b152d0910f1198d176ec7c299dd83
woo-dev-log <sswwhh.dev@gmail.com>
2023-03-21 20:46:09 +0900

    skeleton 구현 및 스타일 수정

--- front/welog-client/src/pages/boardDetail/BoardDetail.tsx
+++ front/welog-client/src/pages/boardDetail/BoardDetail.tsx
@@ -30,6 +30,7 @@ const BoardDetail = () => {
     const [userInfo, setUserInfo] = useRecoilState(loginUser);
     const [updateValue, setUpdateValue] = useRecoilState(boardUpdate);
     const [boardDetail, setBoardDetail] = useState<BoardDetailType[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
     const { boardNo } = useParams();
     const IntBoardNo = Number(boardNo);
     const navigate = useNavigate();
@@ -76,8 +77,10 @@ const BoardDetail = () => {
 
     const getBoardDetail = async () => {
         try {
+            setIsLoading(false);
             const data = await getBoardDetailApi(IntBoardNo);
             setBoardDetail(data);
+            setIsLoading(true);
         } catch (e) {
             ToastError("상세 글 조회를 실패했어요");
             console.error(e);
@@ -91,58 +94,65 @@ const BoardDetail = () => {
     return (
         <>
             <SEO title="상세 글" contents="상세 글" />
-            <button className="boardDetail-backbutton" onClick={() => navigate(-1)}>&lt;&nbsp;&nbsp;이전으로</button>
-            {boardDetail[0] &&
-                <section className="boardDetail-section">
-                    <aside className="boarDetail-boardThumbnail">
-                        <img src={`${ServerImgUrl}${boardDetail[0].boardImgUrl}`} alt="boardThumbnail" />
-                    </aside>
+            {isLoading
+                ? boardDetail[0] &&
+                <>
+                    <button className="boardDetail-backbutton" onClick={() => navigate(-1)}>&lt;&nbsp;&nbsp;이전으로</button>
+                    <section className="boardDetail-section">
+                        <aside className="boarDetail-boardThumbnail">
+                            <img src={`${ServerImgUrl}${boardDetail[0].boardImgUrl}`} alt="boardThumbnail" />
+                        </aside>
 
-                    <div className="boardDetail-container">
-                        <div className="boardDetail-titleContainer">
-                            <h2 className="boardDetail-title">{boardDetail[0].title}</h2>
-                            <div className="board-views">
-                                <img src="/views.svg" alt="click" />
-                                <p>{boardDetail[0].views}</p>
-                            </div>
-                        </div>
-
-                        <div className="boardDetail-writerContainer">
-                            <div className="boardDetail-date">
-                                <p className="boardDetail-rgstrDate">{dayjs(boardDetail[0].rgstrDate).format('YY.MM.DD HH:mm')} 등록</p>
-                                {boardDetail[0].updateDate && <p className="boardDetail-rgstrDate">{dayjs(boardDetail[0].updateDate).format('YY.MM.DD HH:mm')} 수정</p>}
-                            </div>
-                            <div className="boardDetail-userProfile">
-                                <img src={`${ServerImgUrl}${boardDetail[0].imgUrl}`} alt="userProfileImg"
-                                    onClick={() => userBoardOnClick(boardDetail[0].nickname)} />
-                                <p className="boardDetail-nickname" onClick={() => userBoardOnClick(boardDetail[0].nickname)}>
-                                    {boardDetail[0].nickname}
-                                </p>
-                            </div>
-                        </div>
-
-                        <div className="boardDetail-tagContainer">
-                            {boardDetail[0].tags && boardDetail[0].tags.split(",").map((v, i) => (
-                                <p key={i} className="boardDetail-tagBox">{v}</p>
-                            ))}
-                        </div>
-
-                        <Line />
-
-                        <div className="boardDetail-contentsContainer">
-                            <div className="boardDetail-contents" dangerouslySetInnerHTML={{ __html: boardDetail[0].contents }} />
-
-                            {userInfo[0].userNo === boardDetail[0].userNo &&
-                                <div className="boardDetail-deleteBtn">
-                                    <Button onClick={updateBoardOnClick} text="수정" />
-                                    <Button onClick={deleteBoardOnClick} text="삭제" />
+                        <div className="boardDetail-container">
+                            <div className="boardDetail-titleContainer">
+                                <h2 className="boardDetail-title">{boardDetail[0].title}</h2>
+                                <div className="board-views">
+                                    <img src="/views.svg" alt="click" />
+                                    <p>{boardDetail[0].views}</p>
                                 </div>
-                            }
+                            </div>
+
+                            <div className="boardDetail-writerContainer">
+                                <div className="boardDetail-date">
+                                    <p className="boardDetail-rgstrDate">{dayjs(boardDetail[0].rgstrDate).format('YY.MM.DD HH:mm')} 등록</p>
+                                    {boardDetail[0].updateDate && <p className="boardDetail-rgstrDate">{dayjs(boardDetail[0].updateDate).format('YY.MM.DD HH:mm')} 수정</p>}
+                                </div>
+                                <div className="boardDetail-userProfile">
+                                    <img src={`${ServerImgUrl}${boardDetail[0].imgUrl}`} alt="userProfileImg"
+                                        onClick={() => userBoardOnClick(boardDetail[0].nickname)} />
+                                    <p className="boardDetail-nickname" onClick={() => userBoardOnClick(boardDetail[0].nickname)}>
+                                        {boardDetail[0].nickname}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="boardDetail-tagContainer">
+                                {boardDetail[0].tags && boardDetail[0].tags.split(",").map((v, i) => (
+                                    <p key={i} className="boardDetail-tagBox">{v}</p>
+                                ))}
+                            </div>
+
                             <Line />
+
+                            <div className="boardDetail-contentsContainer">
+                                <div className="boardDetail-contents" dangerouslySetInnerHTML={{ __html: boardDetail[0].contents }} />
+
+                                {userInfo[0].userNo === boardDetail[0].userNo &&
+                                    <div className="boardDetail-deleteBtn">
+                                        <Button onClick={updateBoardOnClick} text="수정" />
+                                        <Button onClick={deleteBoardOnClick} text="삭제" />
+                                    </div>
+                                }
+                                <Line />
+                            </div>
+                            <BoardComment IntBoardNo={IntBoardNo} />
                         </div>
-                        <BoardComment IntBoardNo={IntBoardNo} />
-                    </div>
-                </section>
+                    </section>
+                </>
+                : <>
+                    <div className="skeleton-boardDetailBackBtn" />
+                    <section className="skeleton-boardDetailSection" />
+                </>
             }
         </>
     )
